b62c7534527b2ceb3943b9ce44ea422a5cd1e157
thiagodevelopergit <thiagositelp@gmail.com>
2022-01-17 19:09:16 -0300

    versao17012022

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/LoginPage/HomePage';
+
+function App() {
+  return (
+    <div className="app">
+<LoginPage></LoginPage>   
+<HomePage></HomePage>   
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/LoginPage/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css";
+
+const LoginPage = () =>{
+  return (
+<div id="login">
+<h1 className="title">Login do sistema</h1>
+<form action="" className="form">
+    <div className="field">
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
+    </div>
+
+    <div className="field">
+        <label htmlFor="password">Senha</label>
+        <input type="password" name="password" id="password" />
+    </div>
+    <div className="action"><button type="submit">Entrar</button></div>
+    </form>
+</div>
+  );
+}
+
+export default LoginPage;
