5d6598396fb007b3dd9740a7ba5c99140f754fba
thiagodevelopergit <thiagositelp@gmail.com>
2022-01-17 20:18:24 -0300

    versao170120222018

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,24 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 
-const LoginPage = () =>{
+const LoginPage = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log("submit", { email, password });
+};
+
   return (
 <div id="login">
 <h1 className="title">Login do sistema</h1>
-<form action="" className="form">
+<form className="form" onSubmit={handleSubmit}>
     <div className="field">
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
+        <input 
+        type="email" 
+        name="email" 
+        id="email" 
+        value={email}
+         onChange = {(e) => setEmail(e.target.value)}
+         />
     </div>
 
     <div className="field">
         <label htmlFor="password">Senha</label>
-        <input type="password" name="password" id="password" />
+        <input 
+        type="password" 
+        name="password" 
+        id="password"
+        value={password}
+        onChange = {(e) => setPassword(e.target.value)}
+         />
     </div>
-    <div className="action"><button type="submit">Entrar</button></div>
+    <div className="action">
+        <button type="submit">Entrar</button></div>
     </form>
 </div>
   );
-}
+};
 
 export default LoginPage;
